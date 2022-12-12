53337d692bd7e1b29f3666070443473b42c95696
RafaelContact <contactrodrigues21@gmail.com>
2022-12-12 02:29:55 -0300

    variaveis let var e const

---
+++ src/Basico/variaveis/src/index.js
@@ -0,0 +1,6 @@
+let value_confirm = confirm("desja continuar ?")
+
+console.log(value_confirm)
+// retorna valor um valor referente a vesposta que você der tendo valor boleano
+
+let idade = prompt("Digite a sua idade")
---
+++ src/Basico/variaveis/src/var-let-const/index.js
@@ -0,0 +1,9 @@
+var nome = "rafael"
+
+const emailPessoa = "contactrodrigues21@gmail.com"
+
+let telefonePessoa = "13455463465432"
+
+// emailPessoa = "cadu@gmail.com"
+
+console.log(telefonePessoa)
