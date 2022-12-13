2ee14e3b3ef0c9f021b4b6b9b7438aabb6cdf9ee
RafaelContact <contactrodrigues21@gmail.com>
2022-12-13 03:03:13 -0300

    estrutura wile

---
+++ src/Basico/laços/src/wile.js
@@ -0,0 +1,13 @@
+// while(condição){
+    // bloco de execução
+// }
+
+let a = 10
+let b = 15
+
+while(a < b){
+    console.log('Incrementando a variavel a ' + a)
+    a ++;
+}
+
+//  Primeiro a condição, depois o bloco , depois o incremento.
