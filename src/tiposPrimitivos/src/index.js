796b720ab9abeb817c9b35260f79e50f0e02f67a
RafaelContact <contactrodrigues21@gmail.com>
2022-12-13 00:08:42 -0300

    tipo primitivo string , number e boolean

---
+++ src/tiposPrimitivos/src/index.js
@@ -0,0 +1,11 @@
+let nome = "Rafael "
+// tipo string
+
+let numero = 234
+let usuarioVerificado = true
+
+if(usuarioVerificado){
+    console.log("usuário verificado")
+}else{
+    console.log("usuário não é verificado")
+}
