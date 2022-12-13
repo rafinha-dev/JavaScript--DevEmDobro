4c4a39febd90e3b0ea98da41104aebd704d07c33
RafaelContact <contactrodrigues21@gmail.com>
2022-12-13 02:45:48 -0300

    estrutura de repetição for

---
+++ src/Basico/laços/sec/index.js
@@ -0,0 +1,9 @@
+// for(inicializador; condição; incremento)
+//  declaração
+
+for(let i = 0; i < 5; i++){
+    console.log(i)
+}
+// Estrutura de repeteição 
+
+//  inicializa faz o teste da condição e incremento e só depois executa o bloco
