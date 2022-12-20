5ce72e886fa69be6403988135d7709a94a5032fa
RafaelContact <contactrodrigues21@gmail.com>
2022-12-20 12:31:17 -0300

    parâmetros em funções

---
+++ src/Basico/funcao/src/parametro.js
@@ -0,0 +1,8 @@
+function incentivarQuester(nomeQuester = 'Quester'){
+    console.log('Muito bem, ' + nomeQuester + ' você chegou ao JS, continue assim!')
+}
+
+incentivarQuester('Rafael')
+//  se eu não passasse um parâmetro, ela seria imprimida como indefinida, por isso eu posso atribuir um valor padrão do parâmetro e evitar esse comportamento.
+
+//  se eu passar um valor vazio ele irá retornar um valor vazio
