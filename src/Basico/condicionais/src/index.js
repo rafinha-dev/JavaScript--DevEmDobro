2c8f8ffd6655ce330b605da8bdc31f3b4bf377d6
RafaelContact <contactrodrigues21@gmail.com>
2022-12-20 09:43:25 -0300

    estruturas condicionais

---
+++ src/Basico/condicionais/src/index.js
@@ -0,0 +1,12 @@
+ let nomeFilme = 'Vingadores'
+
+ //  Isso são os desvios de condições do código
+ if(nomeFilme === 'Vingadores'){
+    console.log(" É o filme dos vingadores")
+ } else if(nomeFilme === 'Batman vs supermen'){
+    //  esse bloco me permite criar outra contição e ser mais específico
+    console.log("Batman vs supermen")
+ } else{
+    // com isso consigo fazer o ultimo teste
+    console.log("Não é o filme do vingadores")
+ }
