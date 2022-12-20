adb7c75d0b6956e69aaa97f5afb0f73f9c994774
RafaelContact <contactrodrigues21@gmail.com>
2022-12-20 20:41:42 -0300

    correção exercício 5

--- src/Basico/exercício5/src/index.js
+++ src/Basico/exercício5/src/index.js
@@ -2,8 +2,7 @@
 // o número precisa dobrar a cada repetição
 // a mensagem deve conter qual o nomero atual e seu dobro
 
-for (let index = 1; index <= 10 ; index = duble) {
-    var duble = index * 2
-    console.log('O número atual é ' + index + ' e seu dobro é ' + duble)
+for (let index = 1; index <= 10 ; index++) {
+    console.log('O número atual é ' + index + ' e seu dobro é '+  index * 2)
     
 }
