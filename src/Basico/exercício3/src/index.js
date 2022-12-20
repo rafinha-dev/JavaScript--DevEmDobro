a050e4104d39a16d54b577eb587e545fc9b56532
RafaelContact <contactrodrigues21@gmail.com>
2022-12-20 19:22:53 -0300

    exercício 3

---
+++ src/Basico/exercício3/src/index.js
@@ -0,0 +1,8 @@
+let hotel = window.confirm('Gostaria de pagar R$ 50,00 para passara noite na nossa hospedagem?')
+
+if(hotel == true){
+    window.alert('Ótimo! Nós temos as melhores camas de toda a região!');
+} else if(hotel == false){
+
+    window.alert('Que pena! Você parecia ser uma pessoa mais legal')
+} 
