249845e0957176d2fa3040bdee67da6552ca277f
RafaelContact <contactrodrigues21@gmail.com>
2022-12-21 11:16:45 -0300

    hoisting

---
+++ src/intermediario/hoisting/src/index.js
@@ -0,0 +1,22 @@
+/*
+
+console.log(pais)
+var pais = 'Brasil'
+//Esse caso vai ter um problema e não será atribuida, somente declarada, por que ela foi atribuida depois de ser chamada.E ela vai ser indefinida. Indefinida por que a declaração vai para o topo do escopo
+
+*/
+/*
+
+var pais 
+console.log(pais)
+pais = 'Brasil' 
+//Na prática é isso daqui que o javascript está execultando: 
+
+/* 
+console.log(pais)
+let pais = 'Brasil'
+*/ 
+
+console.log(pais)
+const pais = 'Brasil'
+//As variáveis declaradas com let ou com não podem ser lidas ou escritas até que tenham sido totalmente inicializadas
