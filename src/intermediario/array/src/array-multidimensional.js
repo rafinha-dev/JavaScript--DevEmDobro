eaece21abdadb57cc6008792152974776f33742c
RafaelContact <contactrodrigues21@gmail.com>
2022-12-22 10:26:09 -0300

    array multidimensional

---
+++ src/intermediario/array/src/array-multidimensional.js
@@ -0,0 +1,9 @@
+let filmes = [
+        ['Vingadores', 4.5, true], 
+        ['Mulher Maravilha', 4.6, false, ['Gal Gadot', 25]]
+        ]
+// let descricaoFilme = ['Vingadores', 4.5, true]
+
+// console.table(filmes)
+// console.table(filmes[0][0])
+console.table(filmes[1][3][0])
