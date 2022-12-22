a2e575a26cf5117613928b785ef0894bbeac7141
RafaelContact <contactrodrigues21@gmail.com>
2022-12-22 09:53:08 -0300

    arrays

---
+++ src/intermediario/array/src/index.js
@@ -0,0 +1,10 @@
+let filmes = ['Vingadores', 'Mulher Maravilha']
+let avaliacoes = [1, 4.6, 5]
+
+console.log(filmes[0])
+console.log(filmes)
+// se eu tentar imprimir uma posição que não existe ela será indefinida
+//----------------------------
+
+let descricaoFilme = ['Vingadores', 4.5, true]
+console.log(descricaoFilme)
