90a26f54d265c021cd2f65c38c2738f066045f72
RafaelContact <contactrodrigues21@gmail.com>
2023-03-08 11:22:39 -0300

    declarando um objeto

---
+++ src/intermediario/objetos/js/index.js
@@ -0,0 +1,13 @@
+let tenis = { 
+    // chave e valor
+    cor: 'vermelho',
+    tamanho: 38,
+    Estoque: true
+}
+
+tenis.valor = 200
+// inserindo um valor depois de criado o objeto
+
+console.log(tenis)
+// para acessar um valor específico no objeto faça assim
+console.log(tenis.tamanho)
