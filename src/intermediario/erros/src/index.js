01a72298100dfdd097e3f34b7fbb7feb81992306
RafaelContact <contactrodrigues21@gmail.com>
2023-03-10 19:04:51 -0300

    tratando erros no js com erro

---
+++ src/intermediario/erros/src/index.js
@@ -0,0 +1,18 @@
+// let valorBoleto = 100
+
+// console.log(valorBoleto) // se eu olhar no meu console do navegador ele vai me mostrar a linha que está dando erro.
+
+// outro indicativo é que o código pra frente do erro nem é execultado.
+
+// Para pesquisar o erro pesquise o nome da linguagem e a mensagem de erro que está retornando
+
+let inputTelefone = document.getElementById('telefone');
+
+console.log(inputTelefone);
+
+inputTelefone.disabled = true;
+// por que não consigo ver o desabled no console?
+
+function habilitarTelefone(){
+    
+}
