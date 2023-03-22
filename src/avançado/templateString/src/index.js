c6d0f34669ad4c372170e28bfd19bd746e417c34
RafaelContact <contactrodrigues21@gmail.com>
2023-03-22 03:32:54 -0300

    template string

---
+++ src/avançado/templateString/src/index.js
@@ -0,0 +1,5 @@
+const inprimirinformaçõesPessoa = (primeiroNome, ultimoNome, idade) =>{
+    return `Primeiro nome: ${primeiroNome}, Ultimo nome: ${ultimoNome}, Idade ${idade}`
+}
+
+console.log(`${inprimirinformaçõesPessoa("Rafael", "Nunes", 26)}`)
