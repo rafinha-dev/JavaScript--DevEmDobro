e6add1e23465bf4b9cfb87fed9c9b913e2ed4654
RafaelContact <contactrodrigues21@gmail.com>
2023-03-25 05:47:17 -0300

    fetch e metodo .json. Buscando endpoints e encadeamento de funções

---
+++ src/avançado/fetch/src/index.js
@@ -0,0 +1,38 @@
+document.getElementById('tirar-carta').addEventListener('click', () =>{
+    tirarUmaCartaAleatoriaDoBaralho()
+})
+
+async function criarBaralhoEmbaralhado(){
+    const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    // URL da Api precisa estar para consumi-la.
+
+    // o feat é responsavel por buscar os resultados na api, e esses dados serão armazenados em uma variavel. E quando ele termina retorna uma promisse 
+
+    const resposta = await fetch(url)
+    // const json = await resposta.json() no lugar de criar um objeto json da pra retornar direto a resposta
+    return  await resposta.json()
+
+    // ao usar o fatch é preciso usar o await para esperar a resposta da API, e para usar ele é preciso usar o async
+}
+criarBaralhoEmbaralhado()
+
+async function tirarUmaCarta(deck_id){
+    const url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
+    // esse outro endpoint é usado para que se consiga tirar uma carta. Como tirar uma do baralho já tem o deck_id ele entra como parametro.
+    const resposta = await fetch(url)
+    return await resposta.json()
+}
+
+
+async function tirarUmaCartaAleatoriaDoBaralho(){
+    const baralho = await criarBaralhoEmbaralhado()
+    // console.log(baralho.deck_id) para cada f5 vai retornar um baralho novo
+    const carta = await tirarUmaCarta(baralho.deck_id)
+
+    console.log(carta) // aqui ele retorna o objeto que deveria retornar o cards é um array
+    console.log(carta.cards[0].image)
+    const imagemCarta = carta.cards[0].image
+    document.getElementById('carta').src = imagemCarta
+}
+
+tirarUmaCartaAleatoriaDoBaralho()
