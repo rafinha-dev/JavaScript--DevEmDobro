0122fa23dd32e029f51c896d03bf85f14522ee47
RafaelContact <contactrodrigues21@gmail.com>
2023-03-26 22:19:47 -0300

    adicionando node modules e gerenciando pacotes com npm

---
+++ src/avançado/NPM/src/index.js
@@ -0,0 +1,4 @@
+const kindOf = require('kind-of')  // o require é um metodo do node que por parâmetro tem o nome do pactoe.
+const testeDeBoleano = true
+const testDeTexto = 'texto'
+console.log(kindOf(testDeTexto))
